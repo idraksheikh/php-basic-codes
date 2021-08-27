388b8c4591f2a0c6485e44c4872988db750ec5d3
Idrak Sheikh <idraksheikh333@gmail.com>
2021-08-27 16:07:28 +0530

    basic echo and arithematic operators program

---
+++ Program2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arithmatic Operator</title>
+</head>
+<body>
+    <?php
+    echo"<h1 align=center><u>Arithmatic Operator</u></h1>";
+    $a=10;
+    $b=20;
+    echo"<h2>The sum is ".($a+$b)."</h2>";
+    echo"<h2>The Difference is ".($a-$b)."</h2>";
+    echo"<h2>The Multiplication is ".($a*$b)."</h2>";
+    echo"<h2>The division is ".($a/$b)."</h2>";
+    echo"<h2>The remainder is ".($a%$b)."</h2>";
+    ?>
+</body>
+</html>
---
+++ program1.php
@@ -0,0 +1,17 @@
+
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>use of echo basic program</title>
+</head>
+<body>
+<?php
+        echo "<h1>Hello world</h1>";
+        echo "<h2>This is a php Program</h2>";
+    ?>
+</body>
+</html>
